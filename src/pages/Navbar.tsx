aacd56953019cf76ba00389bae741f7058d7e1ec
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-03 22:02:27 +0430

    Add navbar links

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Outlet, NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <>
+      <nav className="navBar">
+        <ul className={`menuNav`}>
+          <li>
+            <NavLink to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/About">
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  );
 }
