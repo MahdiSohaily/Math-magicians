92a2aa63671f5c1a8bb491f3902704ade35b9405
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-03 23:41:22 +0430

    Add quote page styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Home() {
   return (
     <div className="p-8">
-      <h1>Welcome to our page!</h1>
-      <p>
+      <h1 className='mt-4'>Welcome to our page!</h1>
+      <p className='mt-8'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quo amet
         aliquid architecto exercitationem eos, quisquam temporibus ad optio
         placeat doloremque ullam nihil recusandae sit repudiandae nam veniam.
--- src/pages/Quote.tsx
+++ src/pages/Quote.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default function Quote() {
   return (
-    <div className='p-8'>
-      <p className="text-center fs-2">
+    <div className='p-8 quote'>
+      <h1 className="quote-text">
       <strong>
         Mathematics is not about numbers, equations, computations, or
         algorithms: it is about understanding.
       </strong>
       <span className="fs-4"> -William Paul Thurston</span>
-    </p>
+    </h1>
     </div>
   )
 }
