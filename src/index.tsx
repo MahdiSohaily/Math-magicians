3a6794d047ddc9084a325eb058921fc104020228
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-03 22:11:57 +0430

    Add projects routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './pages/Navbar';
 import App from './App';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
+import NoMatch from './pages/noMatch';
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +14,16 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path="calculator" element={<App />} />
+          <Route path="quote" element={<Quote />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </Router>
     <App />
   </React.StrictMode>
 );
--- src/pages/noMatch.tsx
+++ src/pages/noMatch.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function noMatch() {
+export default function NoMatch() {
   return (
     <div>noMatch</div>
   )
