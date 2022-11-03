a38a84783ca274d820eed33c0520cabd7823613f
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-03 22:54:03 +0430

    Add quote page content

--- src/pages/Quote.tsx
+++ src/pages/Quote.tsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Quote() {
   return (
-    <div>Quote</div>
+    <div className='p-8'>
+      <p className="text-center fs-2">
+      <strong>
+        Mathematics is not about numbers, equations, computations, or
+        algorithms: it is about understanding.
+      </strong>
+      <span className="fs-4"> -William Paul Thurston</span>
+    </p>
+    </div>
   )
 }
