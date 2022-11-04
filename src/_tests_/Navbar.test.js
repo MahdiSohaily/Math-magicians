42f14d788491e65df1a8d6709c0f101ee5ac267c
shakerAbuDrais <thecoffeiner@gmail.com>
2022-11-04 17:50:02 +0300

    Add pages tests

---
+++ src/_tests_/Navbar.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Navbar from '../pages/Navbar';
+import { BrowserRouter } from 'react-router-dom';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(
+        <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
