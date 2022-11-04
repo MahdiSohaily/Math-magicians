04e2c5c2df76165361961d23cbd7824dee43b889
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-04 08:47:41 +0430

    Add test case for subtraction

--- src/tests/operation.test.tsx
+++ src/tests/operation.test.tsx
@@ -6,3 +6,8 @@ import operate from '../logic/operate';
 test('adds 3 + 4 to equal 7', () => {
   expect(operate('3', '4', '+')).toBe('7');
 });
+
+// test subtraction operation
+test('Subtract 12 - 4 to equal 8', () => {
+    expect(operate('12', '4', '-')).toBe('8');
+  });
