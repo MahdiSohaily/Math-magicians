10ba276df6cc30166e85827104330a5c8709031f
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-04 17:56:50 +0430

    Add test for calculate file

--- src/tests/operation.test.tsx
+++ src/tests/operation.test.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
 import operate from '../logic/operate';
+
 
 // test sum operation
 test('adds 3 + 4 to equal 7', () => {
@@ -9,15 +8,15 @@ test('adds 3 + 4 to equal 7', () => {
 
 // test subtraction operation
 test('Subtract 12 - 4 to equal 8', () => {
-    expect(operate('12', '4', '-')).toBe('8');
-  });
+  expect(operate('12', '4', '-')).toBe('8');
+});
 
 // test subtraction Multiply
 test('Multiply 4 * 12 to equal 48', () => {
-    expect(operate('12', '4', '*')).toBe('48');
-  });
+  expect(operate('12', '4', '*')).toBe('48');
+});
 
-  // test subtraction dividing
+// test subtraction dividing
 test('Multiply 12 / 4 to equal 3', () => {
-    expect(operate('12', '4', '/')).toBe('3');
-  });
+  expect(operate('12', '4', '/')).toBe('3');
+});
